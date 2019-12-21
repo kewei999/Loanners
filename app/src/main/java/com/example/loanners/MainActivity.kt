8a1e7c88faf94b7c4ffe77c88ca769146aec1722
陈诗贤 <sam_tan9913@hotmail.com>
2019-12-21 23:40:54 +0800

    sign in done sign up half

--- app/src/main/java/com/example/loanners/MainActivity.kt
+++ app/src/main/java/com/example/loanners/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.loanners
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_main.editTextUserName as editTextUserName1
+import kotlinx.android.synthetic.main.activity_sign_up.editTextPassword as editTextPassword1
 
 class MainActivity : AppCompatActivity() {
+
+    private var username = "user1"
+    private var password = "abc123"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,25 @@ class MainActivity : AppCompatActivity() {
             callRegister()
         }
 
+        buttonSignIn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val username = editTextUserName.text.toString()
+        val password = editTextPassword.text.toString()
+        if(username.equals(username)){
+            if(password.equals(password)){
+                Toast.makeText(this, "Log in successful", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Password wrong", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else{
+            Toast.makeText(this, "Username wrong", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun callRegister(){
--- app/src/main/java/com/example/loanners/SignUpActivity.kt
+++ app/src/main/java/com/example/loanners/SignUpActivity.kt
@@ -1,13 +1,47 @@
 package com.example.loanners
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        buttonRegister.setOnClickListener{
+                signup()
+        }
     }
+
+    private fun signup() {
+        val email = editTextEmail.text.toString()
+        var regexEmail = Pattern . compile ("^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\$");
+        var matcher = regexEmail.matcher(email)
+
+        val username = editTextUserName.text.toString()
+        val password = editTextPassword.text.toString()
+        val confirmPassword = editTextconfirmPassword.text.toString()
+        //val type = spinnerUserType.selectedItem.toString()
+
+        if(password != "" && confirmPassword != password){
+            Toast.makeText(this,"Those password didn't match. Try again.",Toast.LENGTH_SHORT).show()
+        }
+        else if(username == "" || email == "" ){
+            Toast.makeText(this,"Form shouldn't left blank.",Toast.LENGTH_SHORT).show()
+        }
+        else if(!matcher.find()){
+            Toast.makeText(this,"Email in wrong format.",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            val intent= Intent(this,DetailsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
